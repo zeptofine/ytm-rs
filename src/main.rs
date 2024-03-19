cc7e1e900cd8a0c7d2a00950c3f214bc0e6b7032
zeptofine <xpsychonauticonx@gmail.com>
2024-03-19 12:25:11 -0400

    add basic ICED items

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{button, column, text, Column};
+
+#[derive(Default)]
+struct Counter {
+    value: i64,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Increment,
+    Decrement,
+}
+
+impl Counter {
+    fn view(&self) -> Column<Message> {
+        column![
+            button("+").on_press(Message::Increment),
+            text(self.value),
+            button("-").on_press(Message::Decrement),
+        ]
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => self.value += 1,
+            Message::Decrement => self.value -= 1,
+        }
+    }
+}
+
+pub fn main() -> iced::Result {
+    iced::run("A cool counter", Counter::update, Counter::view)
 }
